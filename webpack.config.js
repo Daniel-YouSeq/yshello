de50e312a630c0d41c7a087648cff7032117d90a
daniel <daniel.holdbrook@youseq.com>
2022-03-22 10:38:42 +0000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = (env) => {
+  return {
+    entry: './yshello.js',
+    module: {
+      rules: [{
+        test: /.(js|jsx)$/,
+        exclude: ['/node_modules/', '/build/'],
+        use: { loader: 'babel-loader' },
+      }],
+    },
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'js/yshello.min.js',
+      library: {
+        name: 'yshello',
+        type: 'umd',
+      },
+    },
+    plugins: [
+      new ESLintPlugin({
+        extensions: [`js`, `jsx`],
+        exclude: [`node_modules`],
+        emitWarning: true,
+        overrideConfigFile: './.eslintrc.json'
+      }),
+    ],
+    target: 'web'
+  };
+};
